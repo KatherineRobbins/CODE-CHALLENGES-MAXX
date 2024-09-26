63801d57be6c56bd1524f699f73ead757941f747
Katherine Robbins <kak2819@email.vccs.edu>
2024-09-26 17:27:47 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            string toReverse;
+            string stringReverse = "";
+
+            
+            WriteLine("Please enter your string you would like reversed:");
+            toReverse = Console.ReadLine();
+
+            
+
+            for ( int i = toReverse.Length -1; i >=1; i--)
+            {
+                stringReverse += toReverse[i];
+            }
+            WriteLine("Your string in reverse is: " + stringReverse);
+
+
+
+        }
+    }
+}
